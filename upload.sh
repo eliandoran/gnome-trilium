32a55f126a17b268752dfa4cd472afdf0c858a3d
Elian Doran <contact@eliandoran.me>
2023-05-20 21:22:19 +0300

    Switch to Less

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 source ./upload.local.sh
-curl http://localhost:37840/etapi/notes/$NOTE_ID/content \
+./build.sh && curl http://localhost:37840/etapi/notes/$NOTE_ID/content \
   --header "Authorization: $ETAPI_TOKEN" \
   --header "Content-Type: text/plain" \
-  --upload-file style.css
+  --upload-file output.css
